a0f3a12201e45d94a119b6544e156100feb7ee2b
SamNord <samira.boudiaf59@gmail.com>
2020-04-23 00:12:13 +0200

    test bouton select

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 // import { MatMenuModule } from '@angular/material/menu';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 const MaterialAngular =  [
-  MatBadgeModule
+  MatBadgeModule,
+  MatMenuModule
 ]
 
 
